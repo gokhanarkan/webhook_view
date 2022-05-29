22c1ce13a34b08b1173ce96359b65fb85a0591b1
Gokhan Arkan <gokhanarkan@gmail.com>
2022-05-29 23:55:49 +0100

    move head

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Webhook View</title>
+        <meta name="description" content="Webhook View" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
